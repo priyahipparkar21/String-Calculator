71e859b1e900e6392281eb30a0d96b918c535082
unknown <priyahipparkar21@gmail.com>
2024-12-07 03:08:43 +0530

    handled exexception for negative numbers

--- src/add.js
+++ src/add.js
@@ -2,7 +2,11 @@ const add = (input) => {
     var sum =0;   
     const newInput = input.split("\n")
     for(let i=0; i<newInput.length; i++){
-        sum += parseInt(newInput[i])
+        var a = parseInt(newInput[i])
+        if(a<0){
+            throw "negative numbers not allowed"
+        }
+        sum += a
     }
     console.log("result",sum)
 }
